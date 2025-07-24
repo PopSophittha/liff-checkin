48bc7feb7bc0ff97aa255791c9b8dbc49efe0ab9
PopSophittha <sophittha.b@gmail.com>
2025-07-24 12:40:35 +0700

    comment code.js to use GAS web api

--- code.js
+++ code.js
@@ -1,13 +1,15 @@
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile("Index");
-}
+//use web api in GAS
 
-function saveLocation(name, status, lat, lng, note) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('WorkingFile');
-  const now = new Date();
+// function doGet() {
+//   return HtmlService.createHtmlOutputFromFile("Index");
+// }
 
-  sheet.appendRow([now, "", name, status, lat, lng, note]);
+// function saveLocation(name, status, lat, lng, note) {
+//   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('WorkingFile');
+//   const now = new Date();
 
-  // ไม่ต้อง return ข้อความ — ส่งสถานะผ่าน JS ดีกว่า
-  return status; // ส่งแค่ status กลับมา เช่น "Check In"
-}
+//   sheet.appendRow([now, "", name, status, lat, lng, note]);
+
+//   // ไม่ต้อง return ข้อความ — ส่งสถานะผ่าน JS ดีกว่า
+//   return status; // ส่งแค่ status กลับมา เช่น "Check In"
+// }
