d034747a1324843007a6172a1e50ac783a25c9a9
PopSophittha <sophittha.b@gmail.com>
2025-07-24 12:18:02 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile("Index");
+}
+
+function saveLocation(name, status, lat, lng, note) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('WorkingFile');
+  const now = new Date();
+
+  sheet.appendRow([now, "", name, status, lat, lng, note]);
+
+  // ไม่ต้อง return ข้อความ — ส่งสถานะผ่าน JS ดีกว่า
+  return status; // ส่งแค่ status กลับมา เช่น "Check In"
+}
